543709d2d0182b9ace0bfe0339b1b17d612eb149
hyperdiamond0 <st087790@student.spbu.ru>
2022-02-13 14:04:10 +0300

    clickable author links added

---
+++ roguelike_proto/Assets/RoguelikeProto/Scripts/UI/OpenLink.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace RoguelikeProto.Scripts.UI
+{
+    public class OpenLink : MonoBehaviour
+    {
+        public void GoToVfrc008Page()
+        {
+            Application.OpenURL("https://vk.com/vfrc008");
+        }
+        public void GoTohd0Page()
+        {
+            Application.OpenURL("https://vk.com/id167837083");
+        }
+    }
+}
